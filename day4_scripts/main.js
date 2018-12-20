24d96257f6f946d0bf661a13ba338647fcee869f
Glenn <gshoosmith@gmail.com>
2018-12-20 07:27:24 +0000

    final

--- day4_scripts/main.js
+++ day4_scripts/main.js
@@ -2,9 +2,12 @@
 // handler that is called from the Present endpoint
 exports.calling_bird_http = function(data, callback){
 
+  const count = parseInt(data.birds);
+
   let body = "";
   body +="<html><body><h1>Day 4 Present</h1>";
-  body += "<img src = 'https://kscnewmancenter.files.wordpress.com/2012/12/aimgt104.jpg?w=400&h=399'/>";
+  for (let i =0; i < count; i++)
+    body += "<img src = 'https://usercontent2.hubstatic.com/4233451.jpg'/>";
   body += "</body></html>";
 
   const response = {
@@ -15,6 +18,15 @@ exports.calling_bird_http = function(data, callback){
       "Content-Type": "text/html; charset=UTF-8"
     }
   };
+  callback(null, response);
+}
+
+exports.update_client = function(data, callback){
+
+  const client = data.client;
+
+  console.log("Got a client update for:", client.first_name, " ", client.last_name)
+
   callback(null, response);
 }
 
